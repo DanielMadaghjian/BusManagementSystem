e2cbd8ad80bc3e125103a52622321697f80c444a
Daniel Madaghjian <danielmadaghjian@gmail.com>
2022-03-30 17:29:04 +0100

    Initialised stops from stops.txt and edges from transfers.txt and stop_times.txt

--- BusSystemProject/src/DirectedEdge.java
+++ BusSystemProject/src/DirectedEdge.java
@@ -1,4 +1,11 @@
-
 public class DirectedEdge {
+    public int src;
+    public int dest;
+    public double cost;
 
+    public DirectedEdge(int src, int dest, double cost){
+        this.src = src;
+        this.dest = dest;
+        this.cost = cost;
+    }
 }
--- BusSystemProject/src/SystemData.java
+++ BusSystemProject/src/SystemData.java
@@ -21,29 +21,147 @@ public class SystemData {
 		this.transfersFile = filename2;
 		this.stopTimesFile = filename3;
 		stops = new ArrayList<Stop>();
-		//initialiseStops();
+		initialiseStops();
 		V = stops.size();
 		edges = new ArrayList<>(stops.size());
 		for (int i = 0; i < stops.size(); i++) {
             edges.add(new ArrayList<>());
         }
-		//addEdgesFromTransfers();	
-		//addEdgesFromStopTimes();
+		addEdgesFromTransfers();	
+		addEdgesFromStopTimes();
 	}
 	
 	public void initialiseStops()
 	{
+		BufferedReader br;
 		
+		try {
+			br = new BufferedReader(new FileReader(stopsFile));
+			try {
+				String line = br.readLine();
+				line = br.readLine();	
+				int stopId;				
+				String stopName;
+				int i = 0;
+				while(line!= null)
+				{
+					stopId = Integer.parseInt(line.split(",")[0]);					
+					stopName = line.split(",")[2];
+					stops.add(new Stop(stopId, stopName, i));
+					i++;
+					line = br.readLine();
+				}
+				br.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 	
 	public void addEdgesFromTransfers()
 	{
-		
+		BufferedReader br;
+		int src;
+		int dest;
+		double minTransferTime;
+		int transferType;
+		double cost;
+		try {
+			br = new BufferedReader(new FileReader(transfersFile));
+			try {
+				String line = br.readLine();
+				line = br.readLine();		
+				while(line!= null)
+				{					
+					src = Integer.parseInt(line.split(",")[0]);
+					dest = Integer.parseInt(line.split(",")[1]);
+					transferType = Integer.parseInt(line.split(",")[2]);
+									
+					if(transferType == 0)
+					{
+						cost = 2;
+					}
+					else
+					{
+						minTransferTime = Double.parseDouble(line.split(",")[3]);
+						cost = minTransferTime/100.0;
+					}
+					edges.get(getStopIndex(src)).add(new DirectedEdge(src, dest, cost));
+					line = br.readLine();			
+				}
+				br.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 	
 	public void addEdgesFromStopTimes()
 	{
+		BufferedReader br;
+		int currentTripId;
+		int lastTripId;
+		int currentStopId;
+		int lastStopId;
 		
+		double cost = 1;
+		try {
+			br = new BufferedReader(new FileReader(stopTimesFile));
+			try {
+				String line = br.readLine();
+				line = br.readLine();	
+				
+				lastTripId = Integer.parseInt(line.split(",")[0]);
+				lastStopId = Integer.parseInt(line.split(",")[3]);
+				line = br.readLine();
+				while(line!=null)
+				{
+					currentTripId = Integer.parseInt(line.split(",")[0]);
+					if(currentTripId == lastTripId)
+					{
+						currentStopId = Integer.parseInt(line.split(",")[3]);
+						edges.get(getStopIndex(lastStopId)).add(new DirectedEdge(lastStopId, currentStopId, cost));
+						lastStopId = currentStopId;
+						lastTripId = currentTripId;
+					}
+					else
+					{
+						lastStopId = Integer.parseInt(line.split(",")[3]);
+						lastTripId = currentTripId;
+					}
+					line = br.readLine();
+				}
+				br.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	public int getStopIndex(int stopId)
+	{		
+		for(Stop stop: stops)
+		{
+			if(stop.stopId == stopId)
+			{
+				return stop.stopIndex;
+			}
+		}
+		return -1;
 	}
 
 	
